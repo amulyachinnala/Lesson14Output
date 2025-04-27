e8178eedd5f055fa78e2432381f9750be994eb37
amulyachinnala <amulya.chinnala07@gmail.com>
2025-04-27 18:03:58 -0400

    Complete create and read operations with UI

---
+++ Lesson14Output/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Lesson14Output
+//
+//  Created by Amulya on 4/27/25.
+//
+
+import Foundation
+
+class Friend{
+    var name: String
+    var birthday: Date
+    
+    init (name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
